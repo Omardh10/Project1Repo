10339aa583d980615d65d1ec2abe0b1084d00931
Omardh10 <omardaher232@gmail.com>
2026-07-31 22:51:08 +0300

    get NewVersion

--- routes/quastions.js
+++ routes/quastions.js
@@ -1,25 +1,2 @@
-const express = require('express');
-const { verifytoken } = require('../middlware/VerifyTokens');
-const { GetQuestions, GetQuestion, CreateQuestion, UpdateQuestion, DeleteQuestion } = require('../controller/QuastionController');
-const router = express.Router();
 
-// Get All Questions
-router.get('/', verifytoken, GetQuestions)
-
-// Get Single Question
-router.get('/:id', GetQuestion)
-
-// Create New Question
-router.post('/newquestion', verifytoken, CreateQuestion)
-
-// Update Question
-router.patch('/:id', verifytoken, UpdateQuestion)
-
-// Delete Question
-router.delete('/:id', verifytoken, DeleteQuestion)
-
-
-
-
-module.exports = router;
 
